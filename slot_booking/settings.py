39096bfe9e6580a2f7653880b889c946da8fc696
Vasvi Gupta <vasvi2014000000@gmail.com>
2020-06-28 14:54:11 +0530

    Added django-heroku

--- slot_booking/settings.py
+++ slot_booking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 from django.conf.locale.en import formats as en_formats
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -153,6 +154,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
 
 '''AWS_S3_REGION_NAME = 'ap-south-1'
 
