1992a3479df838032c1ccc7514ca2ade117bf105
Vasvi Gupta <vasvi2014000000@gmail.com>
2020-06-28 14:32:42 +0530

    Environment variables

--- slot_booking/settings.py
+++ slot_booking/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=uhq^0aj$mvt=fxby94vmk0a0o8^d_7z9^lc_25r2bvom0!%wb'
+# SECRET_KEY = '=uhq^0aj$mvt=fxby94vmk0a0o8^d_7z9^lc_25r2bvom0!%wb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['raceforspace.herokuapp.com']
 
 # Application definition
 
